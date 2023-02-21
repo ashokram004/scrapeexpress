adcf84ee0d9f289c91d9f6138f6516bbfd47e988
Ashok Ram <77197519+ashokram004@users.noreply.github.com>
2023-02-21 13:20:17 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -4,18 +4,36 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>Enter a URL to get HTML content:</h1>');
+  res.write('<form action="/getHtml" method="post">');
+  res.write('<input type="text" name="url">');
+  res.write('<button type="submit">Get HTML</button>');
+  res.write('</form>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+router.post('/getHtml', async (req, res) => {
+  try {
+    const url = req.body.url;
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    res.send($.html());
+  } catch (error) {
+    res.send(`Error: ${error.message}`);
+  }
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router);
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
